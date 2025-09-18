3d448e033890dc861eb6e08394ef7d5341a6be6a
suzanequotex1-byte <suzanequotex1@gmail.com>
2025-09-18 14:15:28 +0530

    tailwind.config.js

---
+++ usdt checker/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        binance: "#F0B90B"
+      }
+    }
+  },
+  plugins: []
+};
